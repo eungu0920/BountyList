65296934a2804411b188f889886c65d6be695e9a
eungu <eungu0920@naver.com>
2021-07-19 15:57:39 +0900

    CustomCell을 이용해서 Data 할당

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -7,23 +7,60 @@
 
 import UIKit
 
-class BountyViewController: UIViewController {
-
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//    UITableViewDataSource, UITableViewDelegate
+    
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+//    UITableViewDataSource
+//    TableView의 Connections inspector에서 View Controller와 DataSource와 Delegate연결
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bountyList.count
+//        array의 갯수를 리턴
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+//            nil이 아닌경우에 ListCell로 캐스팅 cell.
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg") // 이미지를 가져옴
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLable.text = "\(bountyList[indexPath.row])"
+        
+        return cell
+    }
+    
+    /*
+     if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg") // 이미지를 가져옴
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLable.text = "\(bountyList[indexPath.row])"
+        return cell
+     } else {
+        return UITableViewCell()
+     } 위 guard let else와 동일
+     */
+    
+//    UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> \(indexPath.row)")
+    }
+}
 
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLable: UILabel!
 }
